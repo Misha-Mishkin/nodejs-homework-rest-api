f60f31a60d6ee1981a1e46050ea12e9000f7e597
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-08-17 02:45:32 +0300

    Задание выполнено

---
+++ controllers/contacts/index.js
@@ -0,0 +1,13 @@
+const getAll = require("./getAll");
+const getById = require("./getById");
+const addContact = require("./addContact");
+const removeById = require("./removeDyId");
+const updateById = require("./updateById");
+
+module.exports = {
+  getAll,
+  getById,
+  addContact,
+  removeById,
+  updateById,
+};
